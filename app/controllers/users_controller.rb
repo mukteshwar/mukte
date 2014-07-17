77f6969f9a3473aeb2b031ad27dcd50a074d1e7a
Mukteshwar Prasad <mukteshwarpd@gmail.com>
2014-07-17 18:43:04 +0530

    micropost going on

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-  #@microposts = @user.microposts.paginate(page: params[:page])
+    @microposts = @user.microposts.paginate(page: params[:page])
+ 
   end
 
   def edit
@@ -52,12 +53,12 @@ class UsersController < ApplicationController
 
   private
 
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in."
-    end
-  end
+  # def signed_in_user
+    # unless signed_in?
+      # store_location
+      # redirect_to signin_url, notice: "Please sign in."
+    # end
+  # end
 
   def correct_user
     @user = User.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
-  #has_many :microposts, dependent: :destroy
+  has_many :microposts, dependent: :destroy
   #has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   #has_many :followed_users, through: :relationships, source: :followed
   #has_many :reverse_relationships, foreign_key: "followed_id",
@@ -24,20 +24,44 @@ class User < ActiveRecord::Base
   #end
 
   # def follow!(other_user)
-    # relationships.create!(followed_id: other_user.id)
+  # relationships.create!(followed_id: other_user.id)
   # end
-# 
+  #
   # def unfollow!(other_user)
-    # relationships.find_by_followed_id(other_user.id).destroy
+  # relationships.find_by_followed_id(other_user.id).destroy
   # end
-# 
+  #
   # def feed
-    # Micropost.from_users_followed_by(self)
+  # Micropost.from_users_followed_by(self)
   # end
+
+  # This defines the paypal url for a given product sale
+  def self.paypal_url()
+    values = {
+      :business => 'mukteshwarpd@gmail.com',
+      :cmd => '_cart',
+      :upload => 1,
+      :return => 'http://103.248.84.98:3000/',
+      :invoice => 'mukteshwar00009'
+    }
+
+    values.merge!({
+      "amount_1" => 10,
+      "item_name_1" => 'patanjlihing',
+      "item_number_1" => 1,
+      "quantity_1" => '1'
+    })
+
+    # This is a paypal sandbox url and should be changed for production.
+    # Better define this url in the application configuration setting on environment
+    # basis.
+    "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
+
+  end
 
   private
 
-    def create_remember_token
-      self.remember_token = SecureRandom.urlsafe_base64
-    end
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
