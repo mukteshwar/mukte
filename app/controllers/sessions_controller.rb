5523ad79471fc8a891c5388f2dc2798f804288dc
Mukteshwar Prasad <mukteshwarpd@gmail.com>
2014-07-29 11:24:29 +0530

    heroku restarting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = "TEST"
     user = User.find_by_email(params[:session][:email].downcase)
 
     if user && user.authenticate(params[:session][:password])
